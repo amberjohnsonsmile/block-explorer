e769b50ae9d4358ceaf56ca9f7bec239aa30e70c
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2019-02-08 16:20:52 -0700

    Setting up API fetch request

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+const url = "http://localhost:3001";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      blocks: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ blocks: data });
+        console.log("state", this.state.blocks);
+      });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <p>{this.state.blocks}</p>;
   }
 }
 
