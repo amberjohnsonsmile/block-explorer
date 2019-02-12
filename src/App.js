6f94dc90d6f6f8cc2fc23aab9c5f75595fb7b774
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2019-02-12 15:19:55 -0700

    Bringing in materialize and adding perload animation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from "react"
+import { Col, Row, Preloader } from "react-materialize"
+import "./App.css"
 
-const url = "http://localhost:3001";
+const url = "http://localhost:3001"
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
+      isLoading: true,
       blocks: []
-    };
+    }
   }
 
   componentDidMount() {
     fetch(url)
       .then(response => response.json())
       .then(data => {
-        this.setState({ blocks: data });
-        console.log("state", this.state.blocks);
-      });
+        this.setState({
+          isLoading: false,
+          blocks: data
+        })
+        console.log("state", this.state.blocks)
+      })
   }
 
   render() {
-    return <p>{this.state.blocks}</p>;
+    if (this.state.isLoading) {
+      return (
+        <Row class="preload">
+          <Col s={4}>
+            <Preloader flashing />
+          </Col>
+        </Row>
+      )
+    }
+
+    return <p>{this.state.blocks}</p>
   }
 }
 
-export default App;
+export default App
